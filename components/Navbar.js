686e892e95926fa07a362c3e7d4ecae263b28cc7
Shijoy Bharath <shijoybharath8@gmail.com>
2023-01-30 19:43:05 +0530

    Installed react icons + CSS files created

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import {BsLightningCharge} from 'react-icons/bs'
 
 const Navbar = () => {
   return (
-    <div>
-      <section>
+    <div className={styles.container}>
+      <section className={styles.title}>
+        <BsLightningCharge/>
         <span>Shijoy Bharath</span>
       </section>
-      <section>
+      <section className={styles.links}>
         <div>About</div>
         <div>Experience</div>
         <div>Projects</div>
